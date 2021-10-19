7576906bc41d4dab49fc072085e07e855a3e77d9
Rodrigo Castilho <rcastilho@gmail.com>
2021-10-19 20:22:01 -0300

    CarrosAlugados em DadosUsuario

---
+++ AluguelCarro/ViewComponents/CarrosAlugadosViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using AluguelCarro.AcessoDados.Interfaces;
+using AluguelCarro.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AluguelCarro.ViewComponents {
+    public class CarrosAlugadosViewComponent: ViewComponent {
+        private readonly IUsuarioRepository _usuarioRepositorio;
+        private readonly AppDbContext _context;
+
+        public CarrosAlugadosViewComponent(IUsuarioRepository usuarioRepositorio, AppDbContext context) {
+            _usuarioRepositorio = usuarioRepositorio;
+            _context = context;
+            }
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+            var usuarioLogado = await _usuarioRepositorio.PegarUsuarioLogado(HttpContext.User);
+
+            return View(await _context.Alugueis.Include(a => a.Carro).Where(a => a.UsuarioId == usuarioLogado.Id).ToListAsync());
+            }
+
+        }
+    }
